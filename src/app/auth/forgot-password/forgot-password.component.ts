8f70bb9220ab43e4e6377b37344b72525e24b308
santoshrgaikwad00 <santoshrgaikwad00@gmail.com>
2021-04-16 10:49:54 -0700

    forget, reset form creation

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+// import { AuthenticationService, SharedService } from './../../service/index';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+    public ForgotForm: FormGroup;
+    private errorMsg: any;
 
-  constructor() { }
+    constructor(
+        private fb: FormBuilder,
+        // private authenticationService: AuthenticationService,
+        // private sharedService: SharedService
+      ) {
+        this.ForgotForm = this.createFormGroup();
+    }
+  
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+    }
+
+    //Declare Form Group
+  createFormGroup() {
+    return this.fb.group({
+        email: this.fb.control('',[Validators.required])
+    });
+  }
+
+  onSubmit(value) {
+      console.log(" value =>", value);
+    /* this.authenticationService.forgotPassword(value)
+     .subscribe(response =>
+       {
+         if(response.status == 200){
+           this.sharedService.openSnackBar('Please Check your email for further', 'Success', { duration: 4000 });
+         }
+         else {
+           this.sharedService.openSnackBar('User not found', 'Error', { duration: 4000 });
+         }
+       },
+       error => this.errorMsg = error); */
   }
 
 }
--- src/app/auth/resetpassword/resetpassword.component.ts
+++ src/app/auth/resetpassword/resetpassword.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+// import { AuthenticationService, SharedService } from './../../service/index';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-resetpassword',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetpasswordComponent implements OnInit {
 
-  constructor() { }
+    public user_token : any;
+    public ResetForm: FormGroup;
+    private errorMsg: any;
+  
+    constructor(private router: Router,
+      private route: ActivatedRoute,
+      private fb: FormBuilder,
+      //  private authenticationService: AuthenticationService,
+      // private sharedService: SharedService
+      ) { 
+          this.route.params.subscribe( params => {
+              this.user_token = params.token;
+          });
+      }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.ResetForm = this.fb.group({
+            newpassword: this.fb.control('',[Validators.required]),
+            token: this.fb.control(this.user_token, [Validators.required])
+        });
+    }
+
+    onSubmit(value) {
+      console.log(" value => ", value);
+        /* this.authenticationService.resetPassword(value)
+        .subscribe(response =>
+          {
+            if(response.status == 200){
+              this.sharedService.openSnackBar('New password is set please login', 'Success', { duration: 4000 });
+              this.router.navigate(['/auth/login']);
+            }
+            else {
+              this.sharedService.openSnackBar('New password is not set', 'Error', { duration: 4000 });
+            }
+          },
+          error => this.errorMsg = error); */
+
+    }
 
 }
